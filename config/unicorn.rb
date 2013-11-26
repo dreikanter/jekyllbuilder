80a0a4abedf3dd33cb83bd8de64b3c91d2a9852e
Alex Musayev <alex.musayev@gmail.com>
2013-11-26 23:42:42 +0400

    Unicorn user group specified to fix temp directory access.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ app_root = File.expand_path '../..', __FILE__
 worker_processes 1
 working_directory app_root
 timeout 180
-user 'pi'
+user 'pi', 'pi'
 listen 8000
 pid "#{app_root}/tmp/pids/unicorn.pid"
 stderr_path "#{app_root}/log/unicorn.log"
